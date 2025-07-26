996b8e4c57820f9cfc447f230f0b704bb308c0fa
TamimOp <tamimshadanik@gmail.com>
2025-07-26 19:40:06 +0600

    Refactor GSAP animations in About component for improved readability and performance

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import About from "@/components/About";
+import gsap from "gsap";
 import Cocktails from "@/components/Cocktails";
 import Hero from "@/components/Hero";
+import { ScrollTrigger, SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function Home() {
   return (
--- components/About.tsx
+++ components/About.tsx
@@ -7,31 +7,34 @@ import gsap from "gsap";
 
 const About = () => {
   useGSAP(() => {
-    const titleSplit = SplitText.create("#about h2", { type: "words" });
+    const titleSplit = SplitText.create("#about h2", {
+      type: "words",
+    });
+
     const scrollTimeline = gsap.timeline({
       scrollTrigger: {
         trigger: "#about",
         start: "top center",
       },
     });
+
     scrollTimeline
       .from(titleSplit.words, {
-        yPercent: 100,
         opacity: 0,
         duration: 1,
+        yPercent: 100,
         ease: "expo.out",
         stagger: 0.02,
       })
       .from(
         ".top-grid div, .bottom-grid div",
         {
-          yPercent: 100,
           opacity: 0,
           duration: 1,
           ease: "power1.inOut",
           stagger: 0.04,
         },
-        "-=0.05"
+        "-=0.5"
       );
   });
   return (
