82bbeb449d45d4deabf86a51894d41fc751ae8a9
TamimOp <tamimshadanik@gmail.com>
2025-07-26 20:07:36 +0600

    Add Art component and integrate it into the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import gsap from "gsap";
 import Cocktails from "@/components/Cocktails";
 import Hero from "@/components/Hero";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Art from "@/components/Art";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   );
 }
---
+++ components/Art.tsx
@@ -0,0 +1,69 @@
+import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
+import { useGSAP } from "@gsap/react";
+import { featureLists, goodLists } from "@/constants";
+import Image from "next/image";
+
+const Art = () => {
+  return (
+    <div id="art">
+      <div className="container mx-auto h-full pt-20">
+        <h2 className="will-fade">The ART</h2>
+
+        <div className="content">
+          <ul className="space-y-4 will-fade">
+            {goodLists.map((feature, index) => (
+              <li key={index} className="flex items-center gap-2">
+                <Image
+                  src="/images/check.png"
+                  alt="check"
+                  width={20}
+                  height={20}
+                />
+                <p>{feature}</p>
+              </li>
+            ))}
+          </ul>
+
+          <div className="cocktail-img">
+            <Image
+              src="/images/under-img.jpg"
+              alt="cocktail"
+              width={600}
+              height={600}
+              className="abs-center masked-img size-full object-contain"
+              priority
+            />
+          </div>
+
+          <ul className="space-y-4 will-fade">
+            {featureLists.map((feature, index) => (
+              <li key={index} className="flex items-center justify-start gap-2">
+                <Image
+                  src="/images/check.png"
+                  alt="check"
+                  width={20}
+                  height={20}
+                />
+                <p className="md:w-fit w-60">{feature}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="masked-container">
+          <h2 className="will-fade">Sip-Worthy Perfection</h2>
+          <div id="masked-content">
+            <h3>Made with Craft, Poured with Passion</h3>
+            <p>
+              This isn’t just a drink. It’s a carefully crafted moment made just
+              for you.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Art;
