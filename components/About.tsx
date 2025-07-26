557fdfd2e05a0c3b6c3411726a83dfd812927909
TamimOp <tamimshadanik@gmail.com>
2025-07-26 19:33:27 +0600

    Add GSAP animations to About component for enhanced scroll effects

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,39 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { useGSAP } from "@gsap/react";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
 
 const About = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#about h2", { type: "words" });
+    const scrollTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#about",
+        start: "top center",
+      },
+    });
+    scrollTimeline
+      .from(titleSplit.words, {
+        yPercent: 100,
+        opacity: 0,
+        duration: 1,
+        ease: "expo.out",
+        stagger: 0.02,
+      })
+      .from(
+        ".top-grid div, .bottom-grid div",
+        {
+          yPercent: 100,
+          opacity: 0,
+          duration: 1,
+          ease: "power1.inOut",
+          stagger: 0.04,
+        },
+        "-=0.05"
+      );
+  });
   return (
     <div id="about">
       <div className="mb-16 md:px-0 px-5">
