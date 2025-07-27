43d119a78c78c8c1e13cbc91f235394061b4b28d
TamimOp <tamimshadanik@gmail.com>
2025-07-27 14:27:14 +0600

    Add GSAP animations to Menu component for enhanced visual effects

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -2,11 +2,32 @@
 
 import { allCocktails } from "@/constants";
 import { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import Image from "next/image";
 
 const Menu = () => {
   const contentRef = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  useGSAP(() => {
+    gsap.fromTo("#title", { opacity: 0 }, { opacity: 1, duration: 1 });
+    gsap.fromTo(
+      ".cocktail",
+      { opacity: 0, xPercent: -100 },
+      { xPercent: 0, opacity: 1, duration: 1, ease: "power1.inOut" }
+    );
+    gsap.fromTo(
+      ".details h2",
+      { opacity: 0, yPercent: 100 },
+      { opacity: 1, yPercent: 0, ease: "power1.inOut" }
+    );
+    gsap.fromTo(
+      ".details p",
+      { opacity: 0, yPercent: 100 },
+      { opacity: 1, yPercent: 0, ease: "power1.inOut" }
+    );
+  }, [currentIndex]);
 
   const totalCocktails = allCocktails.length;
 
